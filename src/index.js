e8fbe52d481234dcaed79d1c98c6eb3a23e8b8d2
Ira Novykova <inovykova@axondevgroup.com>
2019-10-21 00:33:28 +0300

    finish project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'styles/reset-form.css';
+import 'styles/index.scss';
+
+import App from 'pages/indexPage/App';
+
+const root = document.getElementById('root');
+
+ReactDOM.render(<App />, root);
---
+++ src/pages/indexPage/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import BigCalendar from 'pages/calendar/BigCalendar';
+
+const App = () => <BigCalendar />;
+
+export default App;
