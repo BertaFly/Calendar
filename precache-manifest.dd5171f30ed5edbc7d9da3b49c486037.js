17d8fd14b1a6e408dfc61fdb120f6789d432fcc7
Ira Novykova <inovykova@axondevgroup.com>
2019-10-21 00:36:50 +0300

    Updates

---
+++ precache-manifest.dd5171f30ed5edbc7d9da3b49c486037.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0302ad7d181617b97049e1c2a8aac510",
+    "url": "/Calendar/index.html"
+  },
+  {
+    "revision": "cd4e0601753592eb124b",
+    "url": "/Calendar/static/css/2.b46dbf60.chunk.css"
+  },
+  {
+    "revision": "ad22b50ab0532e5f6bb1",
+    "url": "/Calendar/static/css/main.bc206575.chunk.css"
+  },
+  {
+    "revision": "cd4e0601753592eb124b",
+    "url": "/Calendar/static/js/2.93c11b71.chunk.js"
+  },
+  {
+    "revision": "ad22b50ab0532e5f6bb1",
+    "url": "/Calendar/static/js/main.78b75112.chunk.js"
+  },
+  {
+    "revision": "fc3b9c46c82451764c55",
+    "url": "/Calendar/static/js/runtime-main.914ab0b7.js"
+  }
+]);
